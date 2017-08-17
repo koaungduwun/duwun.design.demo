3836baac8c40eb372f77cc7830ae40b6e8349d09
Ko Aung <aungmyo.linn@duwun.com.mm>
2017-08-17 07:01:49 +0630

    article page layout almost finished.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,24 @@
 jQuery(document).ready(function () {
 
-    $( "img" ).on( "error", function() {
-        $( this ).attr( "src", "../../images/duwun-no-image-540x265.png" );
+    $( "img" ).each(function() {
+        if((typeof this.naturalWidth != "undefined" &&
+            this.naturalWidth == 0 ) 
+            || this.readyState == 'uninitialized' ) {
+            $(this).attr('src', 'images/duwun-no-image-540x265.png');
+        }  
+        /*
+        $(this).on( "error", function() {
+            alert('found img err');        
+            //$( this ).attr( "src", "images/duwun-no-image-540x265.png" );
+        });
+        */
     });
+    
+    /*
+    $('.navbar-collapse').on('shown.bs.collapse', function() { alert('shown'); });
+    $('.navbar-collapse').on('hidden.bs.collapse', function() { alert('hidden'); });
+    */
+    
     // for hover dropdown menu
     $('ul.nav li.dropdown').hover(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
@@ -17,7 +33,7 @@ jQuery(document).ready(function () {
         slidesToShow: 5,
         slidesToScroll: 5,
         responsive: [{
-            breakpoint: 1140,
+            breakpoint: 1199,
             settings: {
                 slidesToShow: 4,
                 slidesToScroll: 4,
@@ -25,7 +41,7 @@ jQuery(document).ready(function () {
                 dots: true
             }
         }, {
-            breakpoint: 1024,
+            breakpoint: 991,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
@@ -40,7 +56,7 @@ jQuery(document).ready(function () {
 				dots: true
             }
         }, {
-            breakpoint: 480,
+            breakpoint: 575,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
@@ -59,20 +75,20 @@ jQuery(document).ready(function () {
       nextArrow: false,
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 991,
           settings: {
             slidesToShow: 3,
             infinite: true
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 767,
           settings: {
             slidesToShow: 3
           }
         },
         {
-          breakpoint: 480,
+          breakpoint: 575,
           settings: {
             slidesToShow: 3
           }
@@ -83,9 +99,9 @@ jQuery(document).ready(function () {
       ]        
     });
     
-    /*
-    $('.columnist_slick').slick({
-      slidesToShow: 3,
+    
+    $('.peoplespick_slick').slick({
+      slidesToShow: 4,
       slidesToScroll: 1,
       autoplay: true,
       infinite: true,
@@ -94,7 +110,7 @@ jQuery(document).ready(function () {
       nextArrow: false,
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 991,
           settings: {
             slidesToShow: 3,
             infinite: true
@@ -102,14 +118,19 @@ jQuery(document).ready(function () {
         },
         {
           breakpoint: 767,
-          settings: "unslick"
+          settings: {
+            slidesToShow: 3
+          }
+        },        
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 2
+          }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]        
+      ]
     }); 
-    */
+    
     
     //Check to see if the window is top if not then display button
     jQuery(window).scroll(function () {
@@ -127,6 +148,15 @@ jQuery(document).ready(function () {
         return false;
     });
     $(".search_icon").click(function () {
+        
+		if ($('#nav_search_form').is(":visible")) {
+			$('#nav_search_form').fadeOut();
+		} else {
+			$('#nav_search_form').fadeIn();
+		}
+        $("i", $(".search_icon")).toggleClass("fa-search fa-close");
+		$(".search_icon").toggleClass("closing");        
+        /*
 		if ($('.search_bar').is(":visible")) {
 			$('.search_bar').fadeOut();
 		} else {
@@ -134,6 +164,7 @@ jQuery(document).ready(function () {
 		}
         $("i", this).toggleClass("fa-search fa-close");
 		$(this).toggleClass("closing");
+        */
     });
 	/*
     $(document).on('click', '.fa-close', function() {
@@ -153,5 +184,5 @@ jQuery(window).load(function () { // makes sure the whole site is loaded
     $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(100).css({
         'overflow': 'visible'
-    });
+    }); 
 });
